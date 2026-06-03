e8fb065f20b202880c37ddd03f9edcea59de3450
carvalho832-glitch <carvalho832@gmail.com>
2026-06-03 16:26:02 -0300

    Atualizar cache v38 fluxo Shopee afiliado

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_VERSION = 'achou-levou-v37-radar-shopee';
+const CACHE_VERSION = 'achou-levou-v38-shopee-link-afiliado';
 const API_ERRADA = 'https://bot-afiliados-1fvi.onrender.com';
 const API_CORRETA = 'https://bot-afiliados-1fwi.onrender.com';
 
 self.addEventListener('install', (event) => {
-    console.log('Achou Levou interface bot v37 instalada.');
+    console.log('Achou Levou interface bot v38 instalada.');
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('Achou Levou interface bot v37 ativada. Limpando caches antigos.');
+    console.log('Achou Levou interface bot v38 ativada. Limpando caches antigos.');
     event.waitUntil(
         caches.keys()
             .then(keys => Promise.all(keys.map(key => caches.delete(key))))
