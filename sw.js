bc4819914e698de328a2d9ba68230e0a35fd1c36
carvalho832-glitch <carvalho832@gmail.com>
2026-07-25 22:02:11 -0300

    chore: renovar cache para o robô holográfico v60

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-const CACHE_VERSION = 'achou-levou-v59-robo-loader-animado';
+const CACHE_VERSION = 'achou-levou-v60-robo-holografico-realista';
 const API_ERRADA = 'https://bot-afiliados-1fvi.onrender.com';
 const API_CORRETA = 'https://bot-afiliados-1fwi.onrender.com';
 
 self.addEventListener('install', (event) => {
-    console.log('Achou Levou interface v59 instalada.');
+    console.log('Achou Levou interface v60 instalada.');
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('Achou Levou interface v59 ativada. Limpando caches antigos.');
+    console.log('Achou Levou interface v60 ativada. Limpando caches antigos.');
     event.waitUntil(
         caches.keys()
             .then(keys => Promise.all(keys.map(key => caches.delete(key))))
